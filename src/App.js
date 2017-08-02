096514ba819164a388b4a79ee5ce478a1729f3fd
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-08-02 23:47:40 +0100

    Polishment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import Stopwatch from './components/Stopwatch/Stopwatch.js'
 import './App.css';
+import tomatoe from './assets/tomatoe.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="header">
+          <img src={tomatoe} width="60" height="60" alt="Tomatoe"/>
           <h1>Pomodoro</h1>
           <Stopwatch/>
         </div>
